bac9360147b13a1f6956da39ac137cecde00c3aa
bhagya617 <148980352+bhagya617@users.noreply.github.com>
2025-01-01 23:34:14 +0530

    Global.asax.cs

--- MVC/Assignment/Assignment/Assignment/Global.asax.cs
+++ MVC/Assignment/Assignment/Assignment/Global.asax.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace Assignment1
+namespace MVCAssignment
 {
     public class MvcApplication : System.Web.HttpApplication
     {
